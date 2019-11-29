178ecab458a7e281f0912b566af28145c4218f25
Johan Vergeer <johanvergeer@gmail.com>
2019-11-29 10:58:58 +0100

    Use folder as category

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,6 +14,8 @@ TIMEZONE = 'Europe/Amsterdam'
 
 DEFAULT_LANG = 'en'
 
+USE_FOLDER_AS_CATEGORY = True
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
