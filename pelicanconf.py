da471297329150de8f139a5d8586a08bcd46d750
Johan Vergeer <johanvergeer@gmail.com>
2019-11-29 12:20:47 +0100

    Show articles on /articles/..

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,6 +16,10 @@ DEFAULT_LANG = 'en'
 
 USE_FOLDER_AS_CATEGORY = True
 
+# Articles
+ARTICLE_URL = 'articles/{category}/{slug}/'
+ARTICLE_SAVE_AS = 'articles/{category}/{slug}/index.html'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -33,6 +37,7 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
+# Pagination
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
