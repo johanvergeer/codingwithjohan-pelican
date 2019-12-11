f4f228177912051c2c81bac12a8a420118ea544f
Johan Vergeer <johanvergeer@gmail.com>
2019-12-11 21:00:47 +0100

    Add default styles to card BlockProcessor

--- mk_card.py
+++ mk_card.py
@@ -7,6 +7,7 @@ Original code Copyright [Creative Tim](https://www.creative-tim.com/).
 All changes Copyright RedGyro
 License: [MIT](https://opensource.org/licenses/MIT)
 """
+from dataclasses import dataclass
 from typing import Optional, AnyStr, Pattern
 from xml.etree.ElementTree import Element
 
@@ -26,9 +27,26 @@ class CardExtension(Extension):
         md.parser.blockprocessors.register(CardProcessor(md.parser), 'card', 105)
 
 
+@dataclass(frozen=True)
+class CardType:
+    icon: Optional[str]
+    color: Optional[str]
+
+
 class CardProcessor(BlockProcessor):
     CLASSNAME = 'mk-card'
-    RE = re.compile(r'(?:^|\n)!!! card( *\[[a-z-]+=(.*)])*(?:\n|$)')
+    RE = re.compile(r'(?:^|\n)!!! '
+                    r'([a-z]+)'
+                    r'( *\[[a-z-]+=(.*)])*'
+                    r'(?:\n|$)')
+
+    card_types = {
+        'card': CardType(icon=None, color=None),
+        'info': (CardType(icon='info', color='info')),
+        'warning': CardType(icon='warning', color='warning'),
+        'tip': CardType(icon='emoji_objects', color='success'),
+        'important': CardType(icon='notification_important', color='danger')
+    }
 
     def test(self, parent, block):
         sibling = self.lastChild(parent)
@@ -47,8 +65,10 @@ class CardProcessor(BlockProcessor):
         icon = self._get_icon(block)
         subtitle = self._get_subtitle(block)
         color = self._get_color(block)
+        card_type: Optional[CardType] = None
 
         if m:
+            card_type = self._get_card_type(block)
             block = block[m.end():]  # removes the first line
 
         block, the_rest = self.detab(block)
@@ -64,18 +84,21 @@ class CardProcessor(BlockProcessor):
             card.set('class', f'card')
 
             card_header: Optional[Element] = None
-            if title or icon:
+            if title or icon or card_type.icon:
                 card_header = etree.SubElement(card, 'div')
                 card_header.set('class', 'card-header')
 
             card_body = etree.SubElement(card, 'div')
             card_body.set('class', 'card-body')
 
-            if card_header:
+            if card_header is not None:
                 if color:
                     card_header.attrib['class'] += f' card-header-{color}'
+                elif card_type.color:
+                    card_header.attrib['class'] += f' card-header-{card_type.color}'
 
-                if icon:
+                if icon or card_type.icon:
+                    icon = icon if icon is not None else card_type.icon
                     card_header.attrib['class'] += ' card-header-icon'
 
                     card_icon = etree.SubElement(card_header, 'div')
@@ -112,6 +135,13 @@ class CardProcessor(BlockProcessor):
             # line. Insert these lines as the first block of the master blocks
             # list for future processing.
             blocks.insert(0, the_rest)
+
+    def _get_card_type(self, block: str) -> CardType:
+        card_type = self.RE.match(block).group(1)
+        if card_type is not None:
+            return self.card_types.get(card_type)
+        valid_card_types = ', '.join(self.card_types.keys())
+        raise ValueError(f'{card_type} is not a valid card type. use one of {valid_card_types}')
 
     def _get_title(self, block: str) -> Optional[str]:
         return self._get_attribute(block, 'title')
