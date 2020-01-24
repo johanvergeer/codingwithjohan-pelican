99a73a3585fc61fcdb2b825c9dcb01ddb4e7ecd2
Johan Vergeer <johanvergeer@gmail.com>
2020-01-24 16:42:19 +0100

    Include card css classes

--- themes/material-kit/webpack.prod.js
+++ themes/material-kit/webpack.prod.js
@@ -26,7 +26,14 @@ module.exports = merge(common, {
         'highlight', 'hll', 'c', 'err', 'k', 'o', 'ch', 'cm', 'cp', 'cpf', 'c1', 'cs', 'gd', 'ge', 'gr', 'gh', 'gi',
         'go', 'gp', 'gs', 'gu', 'gt', 'kc', 'kd', 'kn', 'kp', 'kr', 'kt', 'm', 's', 'na', 'nb', 'nc', 'no', 'nd',
         'ni', 'ne', 'nf', 'nl', 'nn', 'nt', 'nv', 'ow', 'w', 'mb', 'mf', 'mh', 'mi', 'mo', 'sa', 'sb', 'sc', 'dl',
-        'sd', 's2', 'se', 'sh', 'si', 'sx', 'sr', 's1', 'ss', 'bp', 'fm', 'vc', 'vg', 'vi', 'vm', 'il', 'n'
+        'sd', 's2', 'se', 'sh', 'si', 'sx', 'sr', 's1', 'ss', 'bp', 'fm', 'vc', 'vg', 'vi', 'vm', 'il', 'n',
+        // Cards
+        'mk-card', 'card-header', 'card-header-primary',
+        'card-header-success', 'card-header-warning', 'card-header-info', 'card-header-danger', 'card-header-rose',
+        'card-header-icon', 'card-icon', 'card-body',
+        'material-icons',
+        // Table of contents
+        'toc'
       ]
     })
   ]
